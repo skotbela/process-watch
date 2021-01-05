de9689eb669a86d2c9d4af0471b7941afa1a68ff
skotbela <skotbela@gmail.com>
2021-01-05 10:58:28 +0100

    Add Stream handling

--- src/main/java/com/codecool/processwatch/os/OsProcessSource.java
+++ src/main/java/com/codecool/processwatch/os/OsProcessSource.java
@@ -1,5 +1,6 @@
 package com.codecool.processwatch.os;
 
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import com.codecool.processwatch.domain.Process;
@@ -16,7 +17,19 @@ public class OsProcessSource implements ProcessSource {
      */
     @Override
     public Stream<Process> getProcesses() {
-        return Stream.of(new Process(1,  1, new User("root"), "init", new String[0]),
-                         new Process(42, 1, new User("Codecooler"), "processWatch", new String[] {"--cool=code", "-v"}));
+//        ProcessHandle.allProcesses().forEach(p->{System.out.println(p.info().arguments());});
+//        return Stream.of(new Process(1,  1, new User("root"), "init", new String[0]),
+//                         new Process(42, 1, new User("Codecooler"), "processWatch", new String[] {"--cool=code", "-v"}));
+//         ProcessHandle.allProcesses().forEach(p->{Process process = new Process(p.pid(), p.parent().hashCode(), new User(p.info().user().get()), p.info().command().get(), new String[0]);});
+        ArrayList<Process> processList = new ArrayList<Process>();
+        ProcessHandle.allProcesses().forEach(p->{processList.add(convertProcessHandleToProcess(p));});
+        System.out.println(processList);
+        return processList.stream();
+//        return Stream.of(processList);
     }
+    public Process convertProcessHandleToProcess(ProcessHandle processHandle) {
+//        name converting
+        return new Process(processHandle.pid(), processHandle.parent().hashCode(), new User(processHandle.info().user().orElse("")), processHandle.info().command().orElse(""), new String[0]);
+   }
+
 }
