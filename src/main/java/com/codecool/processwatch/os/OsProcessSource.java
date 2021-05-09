1c510eae66e32327a21d2f96c558ed75af02fe6c
skotbela <skotbela@gmail.com>
2021-05-09 16:27:14 +0200

    Closing branch named skocia

--- src/main/java/com/codecool/processwatch/os/OsProcessSource.java
+++ src/main/java/com/codecool/processwatch/os/OsProcessSource.java
@@ -20,37 +20,28 @@ public class OsProcessSource implements ProcessSource {
      */
     @Override
     public Stream<Process> getProcesses() {
-//        ProcessHandle.allProcesses().forEach(p->{System.out.println(p.info().arguments().map(Object::toString));});
-//        ProcessHandle.allProcesses().forEach(p->{System.out.println(p.info().arguments());});
-//        return Stream.of(new Process(1,  1, new User("root"), "init", new String[0]),
-//                         new Process(42, 1, new User("Codecooler"), "processWatch", new String[] {"--cool=code", "-v"}));
-//         ProcessHandle.allProcesses().forEach(p->{Process process = new Process(p.pid(), p.parent().hashCode(), new User(p.info().user().get()), p.info().command().get(), new String[0]);});
         ArrayList<Process> processList = new ArrayList<Process>();
         ProcessHandle.allProcesses().forEach(p->{processList.add(convertProcessHandleToProcess(p));});
-        //System.out.println(processList);
         return processList.stream();
 
     }
     public Process convertProcessHandleToProcess(ProcessHandle processHandle) {
-//        name converting
         String name=processHandle.info().command().orElse("");
         ArrayList<String> parts=new ArrayList<String>();
         for(String n:name.split("/")){
             parts.add(n);
         }
         int index=0;
-//        System.out.println(parts.size());
         if (parts.size()==1){parts.add("Information is not available");
         index=0;}
         index=parts.size()-1;
-//        System.out.println(parts.get(index));
         String[] emptyStringArray = new String[1];
         emptyStringArray[0] = "Information is not available";
-        //System.out.println(processHandle.info().command().orElse(""));
-//        System.out.println(processHandle.info().arguments().toString());
-//        System.out.println(processHandle.info().startInstant().toString());
-//        System.out.println(new Process(processHandle.pid(), processHandle.parent().hashCode(), new User(processHandle.info().user().orElse("Information is not available")), parts.get(index), processHandle.info().arguments().orElse(emptyStringArray), processHandle.info().startInstant().toString()));
-        return new Process(processHandle.pid(), processHandle.parent().hashCode(), new User(processHandle.info().user().orElse("Information is not available")), parts.get(index), processHandle.info().arguments().orElse(emptyStringArray), processHandle.info().startInstant().get().toString(), processHandle.info().totalCpuDuration().get().toString());
+        return new Process(processHandle.pid(), processHandle.parent().hashCode(),
+                new User(processHandle.info().user().orElse("Information is not available")),
+                parts.get(index), processHandle.info().arguments().orElse(emptyStringArray),
+                processHandle.info().startInstant().get().toString(),
+                processHandle.info().totalCpuDuration().get().toString());
    }
 
 }
